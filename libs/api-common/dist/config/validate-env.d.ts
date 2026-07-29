6927258d5ecf03b65ef443e00cb9f04e84e0e20d
jose.novais@mind-shaker.com <jose.novais@mindshaker.com>
2026-07-29 15:33:35 +0100

    fix: lint

--- libs/api-common/dist/config/validate-env.d.ts
+++ libs/api-common/dist/config/validate-env.d.ts
@@ -1,8 +1,13 @@
 export type EnvLike = Record<string, unknown>;
-export declare function parseEnvBoolean(value: unknown, defaultValue: boolean): boolean;
+export declare function parseEnvBoolean(
+  value: unknown,
+  defaultValue: boolean,
+): boolean;
 export interface ValidateEnvOptions {
-    stringKeys: readonly string[];
-    numberKeys: readonly string[];
-    booleanKeys: readonly string[];
+  stringKeys: readonly string[];
+  numberKeys: readonly string[];
+  booleanKeys: readonly string[];
 }
-export declare function createValidateEnv(options: ValidateEnvOptions): (config: EnvLike) => EnvLike;
+export declare function createValidateEnv(
+  options: ValidateEnvOptions,
+): (config: EnvLike) => EnvLike;
